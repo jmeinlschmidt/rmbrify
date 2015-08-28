00cb82534a35217ba1d5f9029fe2a852c52e61c9
jmeinlschmidt <jakub@meinlschmidt.com>
2015-08-28 15:38:07 +0200

    styles edit

--- Gemfile
+++ Gemfile
@@ -7,11 +7,8 @@ gem 'rails', '4.2.0'
 # gem 'sqlite3'
 gem 'mysql2' 
 
-# Use SCSS for stylesheets
-#gem 'sass-rails', '~> 5.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 gem 'therubyracer', platforms: :ruby
-
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
@@ -45,10 +42,9 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.1.0'
 
-# Use SCSS for stylesheets
-gem 'sass-rails', '>= 3.2'
+gem 'sass-rails'
 
-gem 'bootstrap-sass', '~> 3.3.5'
+gem 'bootstrap-sass'
 
 group :production do
   gem 'unicorn-rails'
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,12 +9,12 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 
-Rails.application.config.assets.precompile += %w( variables.css )
+Rails.application.config.assets.precompile += %w( variables.scss )
 
-Rails.application.config.assets.precompile += %w( home/_layout.css )
-Rails.application.config.assets.precompile += %w( home/tour.css )
+Rails.application.config.assets.precompile += %w( home/_layout.scss )
+Rails.application.config.assets.precompile += %w( home/tour.scss )
 
-Rails.application.config.assets.precompile += %w( notes/_layout.css )
-Rails.application.config.assets.precompile += %w( notes/notes.css )
-Rails.application.config.assets.precompile += %w( notes/about.css )
-Rails.application.config.assets.precompile += %w( notes/settings.css )
+Rails.application.config.assets.precompile += %w( notes/_layout.scss )
+Rails.application.config.assets.precompile += %w( notes/notes.scss )
+Rails.application.config.assets.precompile += %w( notes/about.scss )
+Rails.application.config.assets.precompile += %w( notes/settings.scss )
